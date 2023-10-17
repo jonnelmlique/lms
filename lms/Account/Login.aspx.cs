98e88a65e2f6b57339c5242b8995293f7e617a72
jonnelmlique <jonnelmlique57@gmail.com>
2023-10-17 18:27:42 +0800

    add database package, route to login, asp text

---
+++ lms/Account/Login.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace lms.LOGIN
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ lms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lms.Global" Language="C#" %>
