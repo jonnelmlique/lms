83bf50afa01026359dcdb1d324600c940bf3930e
jonnelmlique <jonnelmlique57@gmail.com>
2023-10-18 11:24:34 +0800

    add logout

---
+++ lms/Account/Logout.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lms.Account
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+
+            Response.Redirect("~/Account/Login.aspx");
+        }
+    }
+}
