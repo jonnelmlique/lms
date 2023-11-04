a383fee5ae5c2923aec9c539a1c2e272fae415f2
Jonnel Lique <jonnelmlique57@gmail.com>
2023-11-04 17:40:51 +0800

    add student list

---
+++ lms/Professor/StudentList.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Professor/professorMasterPage.Master" AutoEventWireup="true" CodeBehind="StudentList.aspx.cs" Inherits="lms.Professor.StudentList" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="../CSS/ProfessorCSS/inviteStudents.css" />
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <div class="reports">
+
+        <div class="report-tbl">
+            <div class="invite-room">
+                <div class="invite-p">
+
+                    <p>List of Student</p>
+                </div>
+                <div class="invite-room-list">
+
+
+                    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+
+
+
+                    <div class="studentlist">
+
+                        <asp:GridView ID="roomlist" runat="server" AutoGenerateColumns="false" EmptyDataText="No Student Found">
+                            <columns>
+<%--                               <asp:BoundField DataField="subjectname" HeaderText="Professor Email" HeaderStyle-CssClass="subj" />--%>
+
+                                <asp:BoundField DataField="studentid" HeaderText="Student ID" HeaderStyle-CssClass="subj" />
+                                <asp:BoundField DataField="StudentEmail" HeaderText="Email" HeaderStyle-CssClass="subj" />
+                                <asp:BoundField DataField="teacherid" HeaderText="Teacher ID" HeaderStyle-CssClass="subj" />
+
+                                <asp:BoundField DataField="TeacherEmail" HeaderText="Professor Email" HeaderStyle-CssClass="subj" />
+
+<%--                                     <asp:BoundField DataField="status" HeaderText="Status" HeaderStyle-CssClass="subj" />--%>
+
+                                <asp:TemplateField HeaderText="" ItemStyle-Width="140px">
+
+                                    <itemtemplate>
+                                        <asp:DropDownList ID="ddlProcess" runat="server" CssClass="action-btn">
+                                            <asp:ListItem Text="Invite" Value="Invite"></asp:ListItem>
+<%--                                            <asp:ListItem Text="DELETE" Value="DELETE"></asp:ListItem>--%>
+                                        </asp:DropDownList>
+                                        <asp:HiddenField ID="hfTnIdPkId" runat="server" Value='<%# Eval("teacherid ") %>' />
+                                        <asp:Button ID="btnUpdateStatus" runat="server" Text="Update Status" CssClass="action-btn" OnClick="btnUpdateStatus_Click"  />
+                                    </itemtemplate>
+                                </asp:TemplateField>
+                            </columns>
+                        </asp:GridView>
+
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ lms/Professor/StudentList.aspx.cs
@@ -0,0 +1,94 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lms.Professor
+{
+    public partial class StudentList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                if (Request.QueryString["roomid"] != null)
+                {
+                    string roomid = Request.QueryString["roomid"];
+                    try
+                    {
+                        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+                        using (MySqlConnection con = new MySqlConnection(connectionString))
+                        {
+                            con.Open();
+                            string query = "SELECT subjectname FROM rooms WHERE roomid = @roomid";
+
+                            using (MySqlCommand command = new MySqlCommand(query, con))
+                            {
+                                command.Parameters.AddWithValue("@roomid", roomid);
+
+                                using (MySqlDataReader reader = command.ExecuteReader())
+                                {
+                                    if (reader.Read())
+                                    {
+                                        string subjectname = reader["subjectname"].ToString();
+                                        Label1.Text = subjectname;
+                                        BindStudentData(roomid);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+
+
+        protected void BindStudentData(string roomid)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+            string loggedinTeacherEmail = Session["LoggedInUserEmail"] as string;
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "SELECT s.studentid, s.email AS StudentEmail, t.teacherid, t.email AS TeacherEmail " +
+                               "FROM student_Info s " +
+                               "INNER JOIN teacher_info t  " + // Added space here
+                               "WHERE t.email = @TeacherEmail";
+
+
+
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@TeacherEmail", loggedinTeacherEmail);
+
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        roomlist.DataSource = dt;
+                        roomlist.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void btnUpdateStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
