c02bf7217373380fff8a0f5eb4c5b45389632b01
Jonnel Lique <jonnelmlique57@gmail.com>
2023-11-13 15:56:04 +0800

    all changes

---
+++ lms/Student/ArchivedClasses.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Student/studentMasterPage.Master" AutoEventWireup="true" CodeBehind="ArchivedClasses.aspx.cs" Inherits="lms.Student.ArchivedClasses" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="../CSS/ProfessorCSS/CreateRoom.css" />
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <div class="room-lists">
+        <asp:Repeater ID="roomRepeater" runat="server">
+            <HeaderTemplate>
+                <ul>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <div class="room-card">
+                    <div class="room-image">
+                        <img class="room-banner" src='<%# "ImageHandler.ashx?roomid=" + Eval("roomid") %>' alt="" />
+                    </div>
+
+                    <div class="room-sched">
+                        <a href='<%# "studentClassroom.aspx?roomid=" + Eval("roomid") %>' class="room-subj"><%# Eval("invitation_subjectname") %></a>
+                        <span class="room-section"><%# Eval("section") + " | " + Eval("schedule") %></span>
+
+                        <div class="room-buttons">
+                            <asp:LinkButton ID="enterroom" runat="server" CssClass="room-btn"
+                                PostBackUrl='<%# "studentClassroom.aspx?roomid=" + Eval("roomid") %>'
+                                Text="Enter Room" />
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </ItemTemplate>
+            <FooterTemplate>
+                </ul>
+            </FooterTemplate>
+        </asp:Repeater>
+
+    </div>
+</asp:Content>
---
+++ lms/Student/ArchivedClasses.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace lms.Student
+{
+    public partial class ArchivedClasses : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+
+
+                    BindRoomData();
+
+
+                }
+                catch (Exception ex)
+                {
+                    ShowErrorMessage("An error occurred while processing your request. Please try again later.");
+                }
+
+            }
+        }
+        protected void BindRoomData()
+        {
+            string studentemail = Session["LoggedInUserEmail"] as string;
+
+            if (string.IsNullOrEmpty(studentemail))
+            {
+                return;
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "SELECT roomid, invitation_subjectname, description, schedule, section, roomstatus FROM invitation_and_rooms_view WHERE studentemail = @studentemail AND status = 'Accepted' AND roomstatus = 'Archived'";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@studentemail", studentemail);
+
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        roomRepeater.DataSource = dt;
+                        roomRepeater.DataBind();
+                    }
+                }
+            }
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            string script = $"Swal.fire({{ icon: 'error', text: '{message}' }})";
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", script, true);
+        }
+        private void ShowSuccessMessage(string message)
+        {
+            string script = $"Swal.fire({{ icon: 'success', text: '{message}' }})";
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", script, true);
+        }
+
+
+    }
+}
